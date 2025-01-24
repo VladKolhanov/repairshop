92f7b39a30c0858833bcaf18a06ecc8eca0244a1
Vlad Kolhanov <vladi.kolganov@gmail.com>
2025-01-24 14:25:40 +0200

    PageProps and settings vscode changed

--- src/types/app.ts
+++ src/types/app.ts
@@ -5,3 +5,11 @@ export type LayoutProps = Readonly<{
 export type TemplateProps = {
   children: React.ReactNode;
 };
+
+export type PageProps<
+  TParams extends Record<string, unknown> | undefined = Record<string, unknown>,
+  TSearchParams extends Record<string, string> | undefined = undefined
+> = {
+  params: TParams;
+  searchParams: Promise<TSearchParams>;
+};
